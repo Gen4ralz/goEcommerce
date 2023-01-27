dbeb7cabc93e4f61472e3b15490654ba4c90063c
Gen4ralz <76762117+Gen4ralz@users.noreply.github.com>
2023-01-28 00:28:37 +0700

    Frontend-Add-Routing

--- goEcommerce-frontend/src/App.tsx
+++ goEcommerce-frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Col, Container, Nav, Navbar, Row } from 'react-bootstrap'
+import { Outlet } from 'react-router-dom'
 import { sampleProducts } from './data'
 
 function App() {
@@ -21,19 +22,7 @@ function App() {
       </header>
       <main>
         <Container className="mt-3">
-          <Row>
-            {sampleProducts.map((product) => (
-              <Col key={product.slug} xs={6} sm={6} md={4} lg={3}>
-                <img
-                  src={product.image}
-                  alt={product.name}
-                  className="product-image"
-                />
-                <h2>{product.name}</h2>
-                <p>${product.price}</p>
-              </Col>
-            ))}
-          </Row>
+          <Outlet />
         </Container>
       </main>
       <footer>
